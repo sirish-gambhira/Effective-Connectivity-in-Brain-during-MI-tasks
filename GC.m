8a91e8904a4074927876c85743ec69f6bbdec515
Sirish <45446751+Sirish07@users.noreply.github.com>
2020-07-18 19:11:36 +0530

    Add files via upload

---
+++ GC.m
@@ -0,0 +1,151 @@
+close all;
+%{
+    Author -  L. Barnett and A. K. Seth
+    Title - MVGC ToolBox
+    Availability - https://users.sussex.ac.uk/~lionelb/MVGC/html/mvgchelp.html
+%}
+
+ntrials   = 1;     % number of trials
+nvars     = 9;
+nobs      = 1750;   % number of observations per trial
+
+regmode   = 'OLS';  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
+icregmode = 'LWR';  % information criteria regression mode ('OLS', 'LWR' or empty for default)
+
+morder    = 'AIC';  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
+momax     = 20;     % maximum model order for model order estimation
+
+acmaxlags = [];   % maximum autocovariance lags (empty for automatic calculation)
+
+tstat     = '';     % statistical test for MVGC:  'F' for Granger's F-test (default) or 'chi2' for Geweke's chi2 test
+alpha     = 0.05;   % significance level for significance test
+mhtc      = 'FDR';  % multiple hypothesis test correction (see routine 'significance')
+
+fs        = 250;    % sample rate (Hz)
+fres      = [];     % frequency resolution (empty for automatic calculation)
+
+seed      = 0;      % random seed (0 for unseeded)
+
+filename = 'r9';
+
+data = load(strcat(filename,'.mat'));
+X = getfield(data,'y');
+
+ptic('\n*** tsdata_to_infocrit\n');
+[AIC,BIC,moAIC,moBIC] = tsdata_to_infocrit(X,momax,icregmode);
+ptoc('*** tsdata_to_infocrit took ');
+
+% Plot information criteria.
+
+figure(1); clf;
+plot_tsdata([AIC BIC]',{'AIC','BIC'},1/fs);
+title('Model order estimation');
+
+amo = 6;
+
+fprintf('\nbest model order (AIC) = %d\n',moAIC);
+fprintf('best model order (BIC) = %d\n',moBIC);
+fprintf('actual model order     = %d\n',amo);
+
+% Select model order.
+
+if     strcmpi(morder,'actual')
+    morder = amo;
+    fprintf('\nusing actual model order = %d\n',morder);
+elseif strcmpi(morder,'AIC')
+    morder = moAIC;
+    fprintf('\nusing AIC best model order = %d\n',morder);
+elseif strcmpi(morder,'BIC')
+    morder = moBIC;
+    fprintf('\nusing BIC best model order = %d\n',morder);
+else
+    fprintf('\nusing specified model order = %d\n',morder);
+end
+
+ptic('\n*** tsdata_to_var... ');
+[A,SIG] = tsdata_to_var(X,morder,regmode);
+ptoc;
+
+% Check for failed regression
+
+assert(~isbad(A),'VAR estimation failed');
+
+% NOTE: at this point we have a model and are finished with the data! - all
+% subsequent calculations work from the estimated VAR parameters A and SIG.
+
+ptic('*** var_to_autocov... ');
+[G,info] = var_to_autocov(A,SIG,acmaxlags);
+ptoc;
+
+% The above routine does a LOT of error checking and issues useful diagnostics.
+% If there are problems with your data (e.g. non-stationarity, colinearity,
+% etc.) there's a good chance it'll show up at this point - and the diagnostics
+% may supply useful information as to what went wrong. It is thus essential to
+% report and check for errors here.
+
+var_info(info,true); % report results (and bail out on error)
+
+ptic('*** autocov_to_pwcgc... ');
+F = autocov_to_pwcgc(G);
+ptoc;
+
+% Check for failed GC calculation
+
+assert(~isbad(F,false),'GC calculation failed');
+
+% Significance test using theoretical null distribution, adjusting for multiple
+% hypotheses.
+
+pval = mvgc_pval(F,morder,nobs,ntrials,1,1,nvars-2,tstat); % take careful note of arguments!
+sig  = significance(pval,alpha,mhtc);
+
+
+
+% Plot time-domain causal graph, p-values and significance.
+
+figure(2); clf;
+subplot(1,3,1);
+plot_pw(F);
+title('Pairwise-conditional GC');
+subplot(1,3,2);
+plot_pw(pval);
+title('p-values');
+subplot(1,3,3);
+plot_pw(sig);
+title(['Significant at p = ' num2str(alpha)])
+
+
+sig(isnan(sig))=0;
+sig = transpose(sig);
+writematrix(sig,strcat(filename,'.csv'));
+
+% For good measure we calculate Seth's causal density (cd) measure - the mean
+% pairwise-conditional causality. We don't have a theoretical sampling
+% distribution for this.
+
+cd = mean(F(~isnan(F)));
+
+fprintf('\ncausal density = %f\n',cd);
+
+ptic('\n*** autocov_to_spwcgc... ');
+f = autocov_to_spwcgc(G,fres);
+ptoc;
+
+% Check for failed spectral GC calculation
+
+assert(~isbad(f,false),'spectral GC calculation failed');
+
+% Plot spectral causal graph.
+
+figure(3); clf;
+plot_spw(f,fs);
+
+fprintf('\nchecking that frequency-domain GC integrates to time-domain GC... \n');
+Fint = smvgc_to_mvgc(f); % integrate spectral MVGCs
+mad = maxabs(F-Fint);
+madthreshold = 1e-5;
+if mad < madthreshold
+    fprintf('maximum absolute difference OK: = %.2e (< %.2e)\n',mad,madthreshold);
+else
+    fprintf(2,'WARNING: high maximum absolute difference = %e.2 (> %.2e)\n',mad,madthreshold);
+end
